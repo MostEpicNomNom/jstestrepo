5c89e3145c581da9b2e72ddc8ae959518da613ea
MostEpicNomNom <mischa.jablonski@gmx.de>
2025-08-13 08:01:48 -0400

    initial commit

---
+++ Scope/scope_lab.js
@@ -0,0 +1,16 @@
+// Global Scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+{
+    // Block Scope
+    var blockVar = "I'm a block-scoped var";
+    let blockLet = "I'm a block-scoped let";
+    const blockConst = "I'm a block-scoped const";
+}
+
+// Global Scope
+console.log(globalVar);
+console.log(globalLet);
+console.log(globalConst);
